42bc61781f9e2ad8a65a5bff8fa34d4bf4febc57
Hyrum Dolan <hyrumdolan@Hyrums-MacBook-Pro.local>
2024-03-30 22:59:22 -0600

    server converted to Apollo and GraphQL

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,83 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+const axios = require('axios');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    searchBooks: async (_, { query }) => {
+      try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+        const { items } = response.data;
+        const bookData = items.map((book) => ({
+          bookId: book.id,
+          authors: book.volumeInfo.authors || ['No author to display'],
+          title: book.volumeInfo.title,
+          description: book.volumeInfo.description,
+          image: book.volumeInfo.imageLinks?.thumbnail || '',
+          link: book.volumeInfo.previewLink,
+        }));
+        return bookData;
+      } catch (error) {
+        console.error('Error fetching books from Google Books API:', error);
+        throw new Error('Failed to fetch books from Google Books API');
+      }
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+        console.log(updatedUser);
+        return updatedUser;
+      }
+      throw new AuthenticationError('Error with Authentication!');
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
